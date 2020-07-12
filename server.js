320cb3df4170877ff0d936a77e759cc054195a03
Hazem Gharib <hazem.gharib@servicerocket.com>
2020-07-13 04:42:39 +0800

    Add IMDB proxy API

--- server.js
+++ server.js
@@ -2,13 +2,32 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
+const dotenv = require('dotenv');
+const axios = require('axios');
 
+dotenv.config();
 const app = express();
 
-// here we are configuring dist to serve app files
+app.get('/api/movies/id/:id', async (req, res) => {
+    const url = `${process.env.API_BASE_URL}?apiKey=${process.env.API_KEY}&i=${req.params.id}`;
+    const apiResponse = await axios.get(url);
+    res.send(apiResponse.data);
+});
+
+app.get('/api/movies/search/:name', async (req, res) => {
+    const url = `${process.env.API_BASE_URL}?apiKey=${process.env.API_KEY}&s=${req.params.name}`;
+    const apiResponse = await axios.get(url);
+    res.send(apiResponse.data);
+});
+
+app.get('/api/movies/title/:title', async (req, res) => {
+    const url = `${process.env.API_BASE_URL}?apiKey=${process.env.API_KEY}&t=${req.params.title}`;
+    const apiResponse = await axios.get(url);
+    res.send(apiResponse.data);
+});
+
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
-// this * route is to serve project on different page routes except root `/`
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
