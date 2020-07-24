8e65ab4ec55acc0c79bdc6c4f95631afcb303fb1
Hazem Gharib <hazem.gharib@servicerocket.com>
2020-07-24 13:10:15 +0800

    Add spinner to Home vue before loading results

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="home">
-        <MovieGrid :cards="moviesList" />
+        <v-progress-circular class="spinner" v-if="!isLoaded" indeterminate size="64"></v-progress-circular>
+        <MovieGrid v-if="isLoaded" :cards="moviesList" />
     </div>
 </template>
 
@@ -19,6 +20,7 @@ export default {
   data() {
     return {
       moviesList: [],
+      isLoaded: false,
     };
   },
   async created() {
@@ -30,6 +32,13 @@ export default {
     ];
 
     this.moviesList = (await searchMovies(chance.pickone(pool), 2019)).data.Search;
+    this.isLoaded = true;
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.spinner {
+    margin: 25rem 56rem;
+}
+</style>
