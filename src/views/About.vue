d3e0fce083d389f87fa8879dff1b2f6d62285317
Hazem Gharib <hazem.gharib@servicerocket.com>
2020-07-24 16:12:27 +0800

    Adding proper information in About vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div class="about">
+    <v-card class="mx-auto" max-width="400">
+        <v-img class="white--text align-end" height="200px" src="http://shorturl.at/elINQ">
+            <v-card-title>Hazem Gharib</v-card-title>
+        </v-img>
+
+        <v-card-subtitle class="pb-0">About Me</v-card-subtitle>
+
+        <v-card-text class="text--primary">
+            <div>Agile Developer</div>
+
+            <div>@ServiceRocket <v-icon class="rocket" color="grey">mdi-rocket</v-icon></div>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-btn color="green" text>
+                <a class="hire-me" href="mailto:hazem.gharib.8@gmail.com">Hire Me</a>
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+    <h3 class="desclaimer">
+        This is app was made with love and a considerable amount of coffee
+        <v-icon color="red">mdi-heart</v-icon>
+    </h3>
+</div>
 </template>
+
 <style lang="scss" scoped>
-  div{
+.about {
     width: fit-content;
-    margin: 20rem auto;
-  }
+    margin: 10rem auto;
+}
+
+.hire-me {
+    color: #4caf50;
+    text-decoration: none;
+}
+
+.desclaimer {
+  margin: 10rem 5rem;
+}
+
+.rocket {
+    transform: rotate(45deg) translate(2px, -6px);
+}
 </style>
